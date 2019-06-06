21b4ab795da27af45c254912d3802b27b8a3f63c
Christos Kokkalis <uberck@gmail.com>
2019-06-06 12:28:57 -0400

    Update2

--- CRUDweb/Models/EmployeeModel.cs
+++ CRUDweb/Models/EmployeeModel.cs
@@ -10,10 +10,22 @@ namespace CRUDweb.Models
     public class EmployeeModel
     {
             
-        [Range(100000, 999999, ErrorMessage = "You need to enter a valid Employee ID")]
+        [Range(100000, 999999, ErrorMessage = "Please enter a valid Employee ID.")]
+
+        [Display(Name = "Employee ID")]
         public int EmployeeID { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+
+        [Required(ErrorMessage = "Please enter a last name.")]
         public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
     }
 }
