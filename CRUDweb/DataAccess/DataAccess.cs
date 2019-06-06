412038a6f2fc88c8951afc5443b8453c71595f8b
Christos Kokkalis <uberck@gmail.com>
2019-06-06 13:56:24 -0400

    Update3

---
+++ CRUDweb/DataAccess/DataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace CRUDweb.Models
+{
+    public class DataAccess
+    {
+        private readonly IConfiguration Configuration;
+        public DataAccess(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IDbConnection GetDapperConnection()
+        {
+            string connectionString = Configuration.GetConnectionString("MassDBString");
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+            return dbConnection;
+
+        }
+    }
+}
